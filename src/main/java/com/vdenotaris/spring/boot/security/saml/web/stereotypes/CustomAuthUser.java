797725ba97e4198c354f6303c85ad367c43f4d82
Saptarshi <saptarshi.polley@zemosolabs.com>
2017-12-08 16:52:44 +0530

    first commit

---
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/stereotypes/CustomAuthUser.java
@@ -0,0 +1,20 @@
+package com.vdenotaris.spring.boot.security.saml.web.stereotypes;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class InMemoryAuthUser extends User {
+    private String emailId;
+
+
+    public InMemoryAuthUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities, String emailId) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.emailId = emailId;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+}
